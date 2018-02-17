6dffbf19052a38d7e6712c2aae12b66c87da449f
fox-tracks <t.andriyanova@yahoo.com>
2018-02-18 00:05:54 +0300

    добавлено меню

--- SimpleStore/SimpleStore.Admin/ClientApp/MainLayout.js
+++ SimpleStore/SimpleStore.Admin/ClientApp/MainLayout.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 const { Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 require('./MainLayout.css');
+
 
 export class MainLayout extends Component {
   render() {
     return (
       <Layout className="MainLayout">
-        <Sider breakpoint="xs" collapsedWidth="0">Sider</Sider>
+        <Sider breakpoint="xs" collapsedWidth="0">
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <SubMenu key="sub1" title='Содержимое'>
+              <Menu.Item key="nav">Навигация</Menu.Item>
+              <Menu.Item key="pages">Страницы</Menu.Item>
+              <Menu.Item key="news">Новости</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title='Каталог'>
+            <Menu.Item key="sections">Разделы</Menu.Item>
+            <Menu.Item key="products">Товары</Menu.Item>
+            <Menu.Item key="import">Импорт</Menu.Item>
+          </SubMenu>
+            <Menu.Item key="orders">
+              <span className="nav-text">Заказы</span>
+            </Menu.Item>
+            <Menu.Item key="settings">
+              <span className="nav-text">Настройки</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Content>Content</Content>
       </Layout>
     );
